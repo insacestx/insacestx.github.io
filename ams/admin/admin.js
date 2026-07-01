92c727587879d585fcea440f1e92c76a0dd16fac
George James Santibanez <george@insaces.com>
2026-07-01 16:09:14 -0500

    Add user email check for admin access

---
+++ ams/admin/admin.js
@@ -0,0 +1,13 @@
+const user = JSON.parse(localStorage.getItem("acesUser") || "{}");
+
+const owners = [
+  "george@insaces.com",
+  "bryan@insaces.com",
+  "jordan@insaces.com",
+  "lanse@insaces.com",
+  "robert@insaces.com"
+];
+
+if (!owners.includes(user.email)) {
+  window.location.href = "/ams/dashboard/dashboard.html";
+}
