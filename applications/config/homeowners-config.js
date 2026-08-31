07e62bf87abb10459d77a61733eaac3018fa1a70
George James Santibanez <george@insaces.com>
2026-08-31 10:55:00 -0500

    Update homeowners-config.js

--- applications/config/homeowners-config.js
+++ applications/config/homeowners-config.js
@@ -9,6 +9,18 @@ export default {
             title_es: "Información de la Propiedad",
             fields: [
                 {
+                    id: "preferredLanguage",
+                    label_en: "Preferred Agent Language",
+                    label_es: "Idioma Preferido del Agente",
+                    type: "select",
+                    required: true,
+                    options: [
+                        { value: "", label_en: "Select...", label_es: "Seleccione..." },
+                        { value: "en", label_en: "English", label_es: "Inglés" },
+                        { value: "es", label_en: "Spanish", label_es: "Español" }
+                    ]
+                },
+                {
                     id: "propertyAddress",
                     label_en: "Property Address",
                     label_es: "Dirección de la Propiedad",
