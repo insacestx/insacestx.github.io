1e5697dcc6acb445621944565726d688541d3e8a
George James Santibanez <george@insaces.com>
2026-08-31 10:56:33 -0500

    Add preferred language option to renters config

--- applications/config/renters-config.js
+++ applications/config/renters-config.js
@@ -16,6 +16,18 @@ export default {
                     required: true
                 },
                 {
+                    id: "preferredLanguage",
+                    label_en: "Preferred Agent Language",
+                    label_es: "Idioma Preferido del Agente",
+                    type: "select",
+                    required: true,
+                    options: [
+                        { value: "", label_en: "Select...", label_es: "Seleccione..." },
+                        { value: "en", label_en: "English", label_es: "Inglés" },
+                        { value: "es", label_en: "Spanish", label_es: "Español" }
+                    ]
+                },
+                {
                     id: "dob",
                     label_en: "Date of Birth",
                     label_es: "Fecha de Nacimiento",
