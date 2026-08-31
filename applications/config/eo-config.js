a6bb2a38f1cb468bf020d75957a169333fc9cfb6
George James Santibanez <george@insaces.com>
2026-08-31 10:53:36 -0500

    Add preferred language field to business info config

--- applications/config/eo-config.js
+++ applications/config/eo-config.js
@@ -9,6 +9,18 @@ export default {
             title_es: "Información del Negocio",
             fields: [
                 { id: "businessName", label_en: "Business Name", label_es: "Nombre del Negocio", type: "text", required: true },
+                {
+                    id: "preferredLanguage",
+                    label_en: "Preferred Agent Language",
+                    label_es: "Idioma Preferido del Agente",
+                    type: "select",
+                    required: true,
+                    options: [
+                        { value: "", label_en: "Select...", label_es: "Seleccione..." },
+                        { value: "en", label_en: "English", label_es: "Inglés" },
+                        { value: "es", label_en: "Spanish", label_es: "Español" }
+                    ]
+                },
                 { id: "industry", label_en: "Professional Field", label_es: "Campo Profesional", type: "text", required: true },
                 { id: "yearsExperience", label_en: "Years of Experience", label_es: "Años de Experiencia", type: "number", required: true }
             ]
