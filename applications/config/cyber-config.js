0ecdb3a6add46d235d14e27068b1111426dd2e28
George James Santibanez <george@insaces.com>
2026-08-31 10:52:57 -0500

    Update cyber-config.js

--- applications/config/cyber-config.js
+++ applications/config/cyber-config.js
@@ -9,6 +9,18 @@ export default {
             title_es: "Perfil del Negocio",
             fields: [
                 { id: "businessName", label_en: "Business Name", label_es: "Nombre del Negocio", type: "text", required: true },
+                {
+                    id: "preferredLanguage",
+                    label_en: "Preferred Agent Language",
+                    label_es: "Idioma Preferido del Agente",
+                    type: "select",
+                    required: true,
+                    options: [
+                        { value: "", label_en: "Select...", label_es: "Seleccione..." },
+                        { value: "en", label_en: "English", label_es: "Inglés" },
+                        { value: "es", label_en: "Spanish", label_es: "Español" }
+                    ]
+                },
                 { id: "industry", label_en: "Industry", label_es: "Industria", type: "text", required: true },
                 { id: "numRecords", label_en: "Estimated Number of Records Stored", label_es: "Número Estimado de Registros Almacenados", type: "number", required: true }
             ]
