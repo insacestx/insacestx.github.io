a2967e7c7da8a3ffecafe36fa28a9772383f5628
George James Santibanez <george@insaces.com>
2026-08-31 10:33:07 -0500

    Update commercial-trucking-config.js

--- applications/config/commercial-trucking-config.js
+++ applications/config/commercial-trucking-config.js
@@ -13,6 +13,18 @@ const commercialTruckingConfig = {
         { id: "businessName", label_en: "Business Name", label_es: "Nombre del Negocio", type: "text", required: true },
         { id: "ownerName", label_en: "Owner Name(s)", label_es: "Nombre(s) del Propietario", type: "text", required: true },
         {
+          id: "preferredLanguage",
+          label_en: "Preferred Agent Language",
+          label_es: "Idioma Preferido del Agente",
+          type: "select",
+          required: true,
+          options: [
+            { value: "", label_en: "Select...", label_es: "Seleccione..." },
+            { value: "en", label_en: "English", label_es: "Inglés" },
+            { value: "es", label_en: "Spanish", label_es: "Español" }
+          ]
+        },
+        {
           id: "businessType",
           label_en: "Business Type",
           label_es: "Tipo de Negocio",
